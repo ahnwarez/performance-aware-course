2d7dc6fbe2b600f2622a8f4283dfe858b2fc4f6f
ahmad anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2024-06-22 09:09:11 +1200

    tidy up tests

--- index.test.ts
+++ index.test.ts
@@ -1,58 +1,60 @@
-import { test, expect } from "vitest";
+import { describe, it, expect } from "vitest";
 
 import { disassemble } from "./disassemble.ts";
 
-test("mov cx, bx", () => {
-  const buffer = Buffer.from("89d9", "hex");
-  expect(disassemble(buffer)?.instruction).toBe("mov cx, bx");
-});
+describe("register to register move", () => {
+  it("mov cx, bx", () => {
+    const buffer = Buffer.from("89d9", "hex");
+    expect(disassemble(buffer)?.instruction).toBe("mov cx, bx");
+  });
 
-test("mov ch, ah", () => {
-  const buffer = Buffer.from("88e5", "hex");
-  expect(disassemble(buffer)?.instruction).toBe("mov ch, ah");
-});
+  it("mov ch, ah", () => {
+    const buffer = Buffer.from("88e5", "hex");
+    expect(disassemble(buffer)?.instruction).toBe("mov ch, ah");
+  });
 
-test("mov dx, bx", () => {
-  const buffer = Buffer.from("89da", "hex");
-  expect(disassemble(buffer)?.instruction).toBe("mov dx, bx");
-});
+  it("mov dx, bx", () => {
+    const buffer = Buffer.from("89da", "hex");
+    expect(disassemble(buffer)?.instruction).toBe("mov dx, bx");
+  });
 
-test("mov si, bx", () => {
-  const buffer = Buffer.from("89de", "hex");
-  expect(disassemble(buffer)?.instruction).toBe("mov si, bx");
-});
+  it("mov si, bx", () => {
+    const buffer = Buffer.from("89de", "hex");
+    expect(disassemble(buffer)?.instruction).toBe("mov si, bx");
+  });
 
-test("mov bx, di", () => {
-  const buffer = Buffer.from("89fb", "hex");
-  expect(disassemble(buffer)?.instruction).toBe("mov bx, di");
-});
+  it("mov bx, di", () => {
+    const buffer = Buffer.from("89fb", "hex");
+    expect(disassemble(buffer)?.instruction).toBe("mov bx, di");
+  });
 
-test("mov al, cl", () => {
-  const buffer = Buffer.from("88c8", "hex");
-  expect(disassemble(buffer)?.instruction).toBe("mov al, cl");
-});
+  it("mov al, cl", () => {
+    const buffer = Buffer.from("88c8", "hex");
+    expect(disassemble(buffer)?.instruction).toBe("mov al, cl");
+  });
 
-test("mov ch, ch", () => {
-  const buffer = Buffer.from("88ed", "hex");
-  expect(disassemble(buffer)?.instruction).toBe("mov ch, ch");
-});
+  it("mov ch, ch", () => {
+    const buffer = Buffer.from("88ed", "hex");
+    expect(disassemble(buffer)?.instruction).toBe("mov ch, ch");
+  });
 
-test("mov bx, ax", () => {
-  const buffer = Buffer.from("89c3", "hex");
-  expect(disassemble(buffer)?.instruction).toBe("mov bx, ax");
-});
+  it("mov bx, ax", () => {
+    const buffer = Buffer.from("89c3", "hex");
+    expect(disassemble(buffer)?.instruction).toBe("mov bx, ax");
+  });
 
-test("mov bx, si", () => {
-  const buffer = Buffer.from("89f3", "hex");
-  expect(disassemble(buffer)?.instruction).toBe("mov bx, si");
-});
+  it("mov bx, si", () => {
+    const buffer = Buffer.from("89f3", "hex");
+    expect(disassemble(buffer)?.instruction).toBe("mov bx, si");
+  });
 
-test("mov sp, di", () => {
-  const buffer = Buffer.from("89fc", "hex");
-  expect(disassemble(buffer)?.instruction).toBe("mov sp, di");
-});
+  it("mov sp, di", () => {
+    const buffer = Buffer.from("89fc", "hex");
+    expect(disassemble(buffer)?.instruction).toBe("mov sp, di");
+  });
 
-test("mov bp, ax", () => {
-  const buffer = Buffer.from("89c5", "hex");
-  expect(disassemble(buffer)?.instruction).toBe("mov bp, ax");
+  it("mov bp, ax", () => {
+    const buffer = Buffer.from("89c5", "hex");
+    expect(disassemble(buffer)?.instruction).toBe("mov bp, ax");
+  });
 });
