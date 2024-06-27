73c917a21aeff82320d9122aa2453e758ad5352f
ahmad anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2024-06-28 09:14:06 +1200

    refactor: Update MOV instruction templates for immediate to register decoding

--- index.test.ts
+++ index.test.ts
@@ -8,64 +8,92 @@ describe('register to register move', () => {
   })
 
   it('mov ch, ah', () => {
-    const buffer = Buffer.from('88e5', 'hex')
     expect(printInstruction(0x88e5)).toBe('mov ch, ah')
   })
 
   it('mov dx, bx', () => {
-    const buffer = Buffer.from('89da', 'hex')
     expect(printInstruction(0x89da)).toBe('mov dx, bx')
   })
 
   it('mov si, bx', () => {
-    const buffer = Buffer.from('89de', 'hex')
     expect(printInstruction(0x89de)).toBe('mov si, bx')
   })
 
   it('mov bx, di', () => {
-    const buffer = Buffer.from('89fb', 'hex')
     expect(printInstruction(0x89fb)).toBe('mov bx, di')
   })
 
   it('mov al, cl', () => {
-    const buffer = Buffer.from('88c8', 'hex')
     expect(printInstruction(0x88c8)).toBe('mov al, cl')
   })
 
   it('mov ch, ch', () => {
-    const buffer = Buffer.from('88ed', 'hex')
     expect(printInstruction(0x88ed)).toBe('mov ch, ch')
   })
 
   it('mov bx, ax', () => {
-    const buffer = Buffer.from('89c3', 'hex')
     expect(printInstruction(0x89c3)).toBe('mov bx, ax')
   })
 
   it('mov bx, si', () => {
-    const buffer = Buffer.from('89f3', 'hex')
     expect(printInstruction(0x89f3)).toBe('mov bx, si')
   })
 
   it('mov sp, di', () => {
-    const buffer = Buffer.from('89fc', 'hex')
     expect(printInstruction(0x89fc)).toBe('mov sp, di')
   })
 
   it('mov bp, ax', () => {
-    const buffer = Buffer.from('89c5', 'hex')
     expect(printInstruction(0x89c5)).toBe('mov bp, ax')
   })
 })
 
 describe('8-bit immediate to register', () => {
   it('mov cl, 12', () => {
-    const buffer = Buffer.from('b10c', 'hex')
     expect(printInstruction(0xb10c)).toBe('mov cl, 12')
   })
 
   it('mov cx, 12', () => {
-    const buffer = Buffer.from('b90c', 'hex')
     expect(printInstruction(0xb90c)).toBe('mov cx, 12')
+  })
+})
+
+describe('16-bit immediate to register', () => {
+  it('mov cx, 12', () => {
+    expect(printInstruction(0xb90d34)).toBe('mov cx, 12')
+  })
+
+  it('mov dx, 339488', () => {
+    expect(printInstruction(0xba6c0f)).toBe('mov dx, 339488')
+  })
+})
+
+describe('source address calculation plus 8-bit displacement', () => {
+  it('mov ah, [bx+si+4]', () => {
+    expect(printInstruction(0x8a6004)).toBe('mov ah, [bx+si+4]')
+  })
+})
+
+describe('source address calculation plus 16-bit displacement', () => {
+  it('mov al, [bx+si+4999]', () => {
+    expect(printInstruction(0x8a60001234)).toBe('mov al, [bx+si+4999]')
+  })
+})
+
+describe('dest address calculation', () => {
+  it('mov [bx+di], cx', () => {
+    expect(printInstruction(0x8909)).toBe('mov [bx+di], cx')
+  })
+
+  it('mov [bp+si], cl', () => {
+    expect(printInstruction(0x8808)).toBe('mov [bp+si], cl')
+  })
+
+  it('mov [bp+si], cl', () => {
+    expect(printInstruction(0x8808)).toBe('mov [bp+si], cl')
+  })
+
+  it('mov [bp], ch', () => {
+    expect(printInstruction(0x886e00)).toBe('mov [bp], ch')
   })
 })
