97c0fb6de2dca4b55c91e9829df126052952ce9b
ahmad anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2024-07-04 15:25:55 +1200

    implpment mov

--- part1/contrib-nodejs-decoder/sim8086_example.js
+++ part1/contrib-nodejs-decoder/sim8086_example.js
@@ -1,10 +1,6 @@
 var addon = require('bindings')('sim8086')
 
-let exampleDisassembly = [
-  0xb8,
-  0x0c,
-  0x00, // mov ax, 0x000c
-]
+let exampleDisassembly = [0xb8, 0x01, 0x00, 0xbb, 0x02, 0x00, 0xb9, 0x03, 0x00, 0xba, 0x04, 0x00, 0xbc, 0x05, 0x00, 0xbd, 0x06, 0x00, 0xbe, 0x07, 0x00, 0xbf, 0x08, 0x00]
 
 const registersState = {
   ax: 0,
@@ -17,30 +13,36 @@ const registersState = {
   di: 0,
 }
 
-for (let index = 0; index < 30; index++) {
-  const reg = addon.getRegisterNameFromOperand({ Index: index, Offset: 0, Count: 0 })
-  console.log(reg)
-}
-
 let offset = 0
 while (offset < exampleDisassembly.length) {
   let dis = addon.decode8086Instruction(exampleDisassembly.slice(offset))
+  if (dis.Type === 0) {
+    console.log('Unrecognized instruction')
+  }
   if (dis.Type != 0) {
     offset += dis.Size
     let op = addon.getMnemonicFromOperationType(dis.Op)
-    console.log('\t', 'Mnemonic', op)
-    for (let i = 0; i < dis.Operands.length; i++) {
-      if (dis.Operands[i].Type == 1) {
-        console.log(dis.Operands[i].Register)
-        console.log('\t', 'Register', addon.getRegisterNameFromOperand(dis.Operands[i].Register))
-      } else if (dis.Operands[i].Type == 2) {
-        console.log('\t', 'Address', dis.Operands[i].Address)
-      } else if (dis.Operands[i].Type == 3) {
-        console.log('\t', 'Immediate', dis.Operands[i].Immediate.Value)
-      }
-    }
-  } else {
-    console.log('Unrecognized instruction')
+    printInstruction(op, dis.Operands)
+    simulate(op, dis.Operands)
+    printRegistersState(registersState)
   }
-  console.log('--------------------------------------------------------------------------------')
+}
+
+function simulate(mnemonic, operands) {
+  const destIndex = 0
+  const srcIndex = 1
+  if (mnemonic == 'mov') {
+    if (operands[destIndex].Type == 1 && operands[srcIndex].Type == 3) {
+      const dest = addon.getRegisterNameFromOperand(operands[destIndex].Register)
+      registersState[dest] = operands[srcIndex].Immediate.Value
+    }
+  }
+}
+
+function printInstruction(mnemonic, operands) {
+  console.log(`${mnemonic} ${operands.map((operand) => (operand.Type == 1 ? addon.getRegisterNameFromOperand(operand.Register) : operand.Type == 2 ? operand.Address : operand.Immediate.Value)).join(', ')}`)
+}
+
+function printRegistersState(registersState) {
+  console.table(registersState)
 }
