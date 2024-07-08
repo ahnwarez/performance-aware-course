0f1443ef904dbb2e9d4b2fc25ec1d410d767b71f
ahmad anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2024-07-08 18:58:32 +1200

    Refactor sim8086_example.js for improved performance and readability

--- part1/contrib-nodejs-decoder/sim8086_example.js
+++ part1/contrib-nodejs-decoder/sim8086_example.js
@@ -1,6 +1,9 @@
 var addon = require('bindings')('sim8086')
-
-let exampleDisassembly = [0xb8, 0x01, 0x00, 0xbb, 0x02, 0x00, 0xb9, 0x03, 0x00, 0xba, 0x04, 0x00, 0xbc, 0x05, 0x00, 0xbd, 0x06, 0x00, 0xbe, 0x07, 0x00, 0xbf, 0x08, 0x00]
+//b903 00bb e803 83c3 0a83 e901 75f8
+let exampleDisassembly = [
+  0xb9, 0x03, 0x00, 0xbb, 0xe8, 0x03, 0x83, 0xc3, 0x0a, 0x83, 0xe9, 0x01, 0x75,
+  0xf8,
+]
 
 const registersState = {
   ax: 0,
@@ -11,6 +14,12 @@ const registersState = {
   bp: 0,
   si: 0,
   di: 0,
+  ip: 0,
+}
+
+const Flags = {
+  ZF: 0,
+  SF: 0,
 }
 
 let offset = 0
@@ -23,26 +32,103 @@ while (offset < exampleDisassembly.length) {
     offset += dis.Size
     let op = addon.getMnemonicFromOperationType(dis.Op)
     printInstruction(op, dis.Operands)
-    simulate(op, dis.Operands)
+    simulate(op, dis.Operands, dis.Size)
     printRegistersState(registersState)
   }
 }
 
-function simulate(mnemonic, operands) {
+function simulate(mnemonic, operands, size) {
+  registersState.ip += size
+  executeInstruction(mnemonic, operands)
+}
+
+function executeInstruction(mnemonic, operands) {
   const destIndex = 0
   const srcIndex = 1
+  const destType = operands[destIndex].Type
+  const srcType = operands[srcIndex].Type
+  const src =
+    srcType == 3
+      ? operands[srcIndex].Immediate.Value
+      : srcType === 1
+      ? registersState[
+          addon.getRegisterNameFromOperand(operands[srcIndex].Register)
+        ]
+      : undefined
+
+  const dest =
+    destType !== 3
+      ? addon.getRegisterNameFromOperand(operands[destIndex].Register)
+      : undefined
+  if (mnemonic == 'jne') {
+    const value = operands[0].Immediate.Value
+    registersState['ip'] += value
+  }
+
   if (mnemonic == 'mov') {
     if (operands[destIndex].Type == 1 && operands[srcIndex].Type == 3) {
-      const dest = addon.getRegisterNameFromOperand(operands[destIndex].Register)
+      const dest = addon.getRegisterNameFromOperand(
+        operands[destIndex].Register
+      )
       registersState[dest] = operands[srcIndex].Immediate.Value
+    } else if (operands[destIndex].Type == 1 && operands[srcIndex].Type == 1) {
+      const src = addon.getRegisterNameFromOperand(operands[srcIndex].Register)
+      const dest = addon.getRegisterNameFromOperand(
+        operands[destIndex].Register
+      )
+      registersState[dest] = registersState[src]
+    }
+  } else if (mnemonic == 'add') {
+    if (destType == 1) {
+      registersState[dest] += src
+
+      Flags.ZF = registersState[dest] === 0 ? 1 : 0
+      Flags.SF = registersState[dest] & 0x8000 ? 1 : 0
+    }
+  } else if (mnemonic == 'sub') {
+    if (destType == 1) {
+      registersState[dest] -= src
+
+      Flags.ZF = registersState[dest] === 0 ? 1 : 0
+      Flags.SF = registersState[dest] & 0x8000 ? 1 : 0
+    }
+  } else if (mnemonic === 'cmp') {
+    if (destType == 1) {
+      const dest = addon.getRegisterNameFromOperand(
+        operands[destIndex].Register
+      )
+      const src =
+        srcType == 3
+          ? operands[srcIndex].Immediate.Value
+          : registersState[
+              addon.getRegisterNameFromOperand(operands[srcIndex].Register)
+            ]
+      const result = registersState[dest] - src
+
+      Flags.ZF = result === 0 ? 1 : 0
+      Flags.SF = result < 0 ? 1 : 0
     }
   }
 }
 
 function printInstruction(mnemonic, operands) {
-  console.log(`${mnemonic} ${operands.map((operand) => (operand.Type == 1 ? addon.getRegisterNameFromOperand(operand.Register) : operand.Type == 2 ? operand.Address : operand.Immediate.Value)).join(', ')}`)
+  console.log(
+    `${mnemonic} ${operands
+      .map((operand) =>
+        operand.Type == 1
+          ? addon.getRegisterNameFromOperand(operand.Register)
+          : operand.Type == 2
+          ? operand.Address
+          : operand.Type == 3
+          ? operand.Immediate.Value
+          : undefined
+      )
+      .filter(Boolean)
+      .join(', ')}`
+  )
 }
 
 function printRegistersState(registersState) {
   console.table(registersState)
+  console.table(Flags)
 }
