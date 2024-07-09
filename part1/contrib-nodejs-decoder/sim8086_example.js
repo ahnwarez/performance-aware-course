34d924e89df5b0c59417f915ab512ee3260a32bd
ahmad anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2024-07-09 14:08:15 +1200

    tidy up and re-write

--- part1/contrib-nodejs-decoder/sim8086_example.js
+++ part1/contrib-nodejs-decoder/sim8086_example.js
@@ -1,154 +1,200 @@
-var addon = require('bindings')('sim8086')
-//c706 e803 0100 c706 ea03 0200 c706 ec03
-//0300 c706 ee03 0400 bbe8 03c7 4704 0a00
-//8b1e e803 8b0e ea03 8b16 ec03 8b2e ee03
-let exampleDisassembly = [
-  0xc7, 0x06, 0xe8, 0x03, 0x01, 0x00, 0xc7, 0x06, 0xea, 0x03, 0x02, 0x00, 0xc7,
-  0x06, 0xec, 0x03, 0x03, 0x00, 0xc7, 0x06, 0xee, 0x03, 0x04, 0x00, 0xbb, 0xe8,
-  0x03, 0xc7, 0x47, 0x04, 0x0a, 0x00, 0x8b, 0x1e, 0xe8, 0x03, 0x8b, 0x0e, 0xea,
-  0x03, 0x8b, 0x16, 0xec, 0x03, 0x8b, 0x2e, 0xee, 0x03,
-]
+const { log } = require('console')
 
-const registersState = {
-  ax: 0,
-  bx: 0,
-  cx: 0,
-  dx: 0,
-  sp: 0,
-  bp: 0,
-  si: 0,
-  di: 0,
-  ip: 0,
-}
+const addon = require('bindings')('sim8086')
 
-const Flags = {
-  ZF: 0,
-  SF: 0,
-}
-
-const memory = new Uint8Array(0xffff)
-
-let offset = 0
-while (offset < exampleDisassembly.length) {
-  const dis = addon.decode8086Instruction(exampleDisassembly.slice(offset))
-  if (dis.Type === 0) {
-    console.log('Unrecognized instruction')
-  }
-  if (dis.Type != 0) {
-    offset += dis.Size
-    registersState.ip = offset
-    const op = addon.getMnemonicFromOperationType(dis.Op)
-    printInstruction(op, dis.Operands)
-    simulate(op, dis.Operands, (ip) => {
-      offset += ip
-    })
-    printRegistersState(registersState)
+function createCPU() {
+  return {
+    registers: {
+      ax: 0,
+      bx: 0,
+      cx: 0,
+      dx: 0,
+      sp: 0,
+      bp: 0,
+      si: 0,
+      di: 0,
+      ip: 0,
+    },
+    flags: {
+      zf: 0,
+      sf: 0,
+    },
+    memory: new Uint8Array(0xffff),
   }
 }
 
-function simulate(mnemonic, operands, cb) {
-  executeInstruction(mnemonic, operands, cb)
+function setRegister(cpu, name, value) {
+  cpu.registers[name] = value & 0xffff
 }
 
-function executeInstruction(mnemonic, operands, cb) {
-  const destIndex = 0
-  const srcIndex = 1
-  const destType = operands[destIndex].Type
-  const srcType = operands[srcIndex].Type
-  const src =
-    srcType == 3
-      ? operands[srcIndex].Immediate.Value
-      : srcType === 1
-      ? registersState[
-          addon.getRegisterNameFromOperand(operands[srcIndex].Register)
-        ]
-      : undefined
+function getRegister(cpu, name) {
+  return cpu.registers[name]
+}
 
-  if (mnemonic == 'jne') {
-    const value = operands[0].Immediate.Value
-    if (Flags.ZF === 0) {
-      // then jump
-      cb(value)
-    }
+function setMemory(cpu, address, value) {
+  cpu.memory[address] = value & 0xff
+  cpu.memory[address + 1] = (value >> 8) & 0xff
+}
+
+// little endian
+function getMemory(cpu, address) {
+  return cpu.memory[address] | (cpu.memory[address + 1] << 8)
+}
+
+function updateFlags(cpu, value) {
+  cpu.flags.zf = value === 0 ? 1 : 0
+  const signMask = 0x8000
+  cpu.flags.sf = value & signMask ? 1 : 0
+}
+
+function getOperandValue(cpu, operand) {
+  switch (operand.Type) {
+    case 1: // Register
+      return getRegister(
+        cpu,
+        addon.getRegisterNameFromOperand(operand.Register)
+      )
+      break
+    case 2: // Memory
+      return getMemory(cpu, operand.Address.Displacement)
+      break
+    case 3:
+      return operand.Immediate.Value
+    default:
+      throw new Error(`Unknown operand type: ${operand.Type}`)
   }
+}
 
-  if (mnemonic == 'mov') {
-    if (operands[0].Type === 2) {
-      const dest = operands[0].Address.Displacement
-      // 16 bit value
-      const src = operands[1].Immediate.Value
-      // divide the value into low and high bytes
-      const low = src & 0xff
-      const high = (src & 0xff00) >> 8
-      memory[dest] = low
-      memory[dest + 1] = high
-    } else if (operands[destIndex].Type == 1 && operands[srcIndex].Type == 3) {
-      const dest = addon.getRegisterNameFromOperand(
-        operands[destIndex].Register
+function PrintOperand(cpu, operand) {
+  switch (operand.Type) {
+    case 0: // None
+      return ''
+    case 1: // Register
+      return addon.getRegisterNameFromOperand(operand.Register)
+    case 2: {
+      // Memory
+      const displacement = operand.Address.Displacement
+      const base1 = addon.getRegisterNameFromOperand(
+        operand.Address.Terms[0].Register
       )
-      registersState[dest] = operands[srcIndex].Immediate.Value
-    } else if (operands[destIndex].Type == 1 && operands[srcIndex].Type == 1) {
-      const src = addon.getRegisterNameFromOperand(operands[srcIndex].Register)
-      const dest = addon.getRegisterNameFromOperand(
-        operands[destIndex].Register
+      const base2 = addon.getRegisterNameFromOperand(
+        operand.Address.Terms[1].Register
       )
-      registersState[dest] = registersState[src]
+      return `[${[base1, base2, displacement]
+        .filter((r) => r !== '')
+        .join(' + ')}]`
     }
-  } else if (mnemonic == 'add') {
-    if (destType == 1) {
-      registersState[dest] += src
+    case 3:
+      return operand.Immediate.Value
+    default:
+      throw new Error(`Unknown operand type: ${operand.Type}`)
+  }
+}
 
-      Flags.ZF = registersState[dest] === 0 ? 1 : 0
-      Flags.SF = registersState[dest] & 0x8000 ? 1 : 0
-    }
-  } else if (mnemonic == 'sub') {
-    if (destType == 1) {
-      registersState[dest] -= src
-
-      Flags.ZF = registersState[dest] === 0 ? 1 : 0
-      Flags.SF = registersState[dest] & 0x8000 ? 1 : 0
-    }
-  } else if (mnemonic === 'cmp') {
-    if (destType == 1) {
-      const dest = addon.getRegisterNameFromOperand(
-        operands[destIndex].Register
+function setOperandValue(cpu, operand, value) {
+  switch (operand.Type) {
+    case 1: // Register
+      setRegister(
+        cpu,
+        addon.getRegisterNameFromOperand(operand.Register),
+        value
       )
-      const src =
-        srcType == 3
-          ? operands[srcIndex].Immediate.Value
-          : registersState[
-              addon.getRegisterNameFromOperand(operands[srcIndex].Register)
-            ]
-      const result = registersState[dest] - src
+      break
+    case 2: // Memory
+      setMemory(cpu, operand.Address.Displacement, value)
+      break
+    default:
+      throw new Error(`Unknown operand type: ${operand.Type}`)
+  }
+}
 
-      Flags.ZF = result === 0 ? 1 : 0
-      Flags.SF = result < 0 ? 1 : 0
+const instructions = {
+  mov: (cpu, operands) => {
+    const value = getOperandValue(cpu, operands[1])
+    setOperandValue(cpu, operands[0], value)
+  },
+  add: (cpu, operands) => {
+    const src = getOperandValue(cpu, operands[1])
+    const dest = getOperandValue(cpu, operands[0])
+    const result = (dest + src) & 0xffff
+    setOperandValue(cpu, operands[0], result)
+    updateFlags(cpu, result)
+  },
+
+  sub: (cpu, operands) => {
+    const src = getOperandValue(cpu, operands[1])
+    const dest = getOperandValue(cpu, operands[0])
+    const result = (dest - src) & 0xffff
+    setOperandValue(cpu, operands[0], result)
+    updateFlags(cpu, result)
+  },
+  cmp: (cpu, operands) => {
+    const src = getOperandValue(cpu, operands[1])
+    const dest = getOperandValue(cpu, operands[0])
+    const result = (dest - src) & 0xffff
+    updateFlags(cpu, result)
+  },
+  jne: (cpu, operands) => {
+    if (cpu.zf === 0) {
+      cpu.registers.ip += operands[0].Immediate.Value
     }
+  },
+}
+
+function executeInstruction(cpu, mnemonic, operands) {
+  if (instructions[mnemonic]) {
+    instructions[mnemonic](cpu, operands)
+  } else {
+    console.log(`Unrecognized instruction: ${mnemonic}`)
+  }
+}
+
+function decodeAndExecute(cpu, instructionBytes) {
+  let offset = 0
+  while (offset < instructionBytes.length) {
+    const decodedInstruction = addon.decode8086Instruction(
+      instructionBytes.slice(offset)
+    )
+    if (decodedInstruction.Type === 0) {
+      console.log('Unrecognized instruction')
+      break
+    }
+
+    offset += decodedInstruction.Size
+    cpu.registers.ip = offset
+
+    const mnemonic = addon.getMnemonicFromOperationType(decodedInstruction.Op)
+
+    printInstruction(mnemonic, decodedInstruction.Operands)
+    executeInstruction(cpu, mnemonic, decodedInstruction.Operands)
+    printCPUState(cpu)
   }
 }
 
 function printInstruction(mnemonic, operands) {
   console.log(
     `${mnemonic} ${operands
-      .map((operand) =>
-        operand.Type == 1
-          ? addon.getRegisterNameFromOperand(operand.Register)
-          : operand.Type == 2
-          ? operand.Address
-          : operand.Type == 3
-          ? operand.Immediate.Value
-          : undefined
-      )
-      .filter(Boolean)
+      .map((operand) => PrintOperand(cpu, operand))
       .join(', ')}`
   )
 }
 
-function printRegistersState(registersState) {
-  console.table(registersState)
-  console.log(memory[0x03e8])
-  console.log(memory[0x03ea])
-  console.log(memory[0x03ec])
-  console.log(memory[0x03ee])
-  // console.table(Flags)
+function printCPUState(cpu) {
+  console.table(cpu.registers)
+  // console.log('Memory at 0x03e8:', getMemory(cpu, 0x03e8))
+  // console.log('Memory at 0x03ea:', getMemory(cpu, 0x03ea))
+  // console.log('Memory at 0x03ec:', getMemory(cpu, 0x03ec))
+  // console.log('Memory at 0x03ee:', getMemory(cpu, 0x03ee))
+  console.table(cpu.flags)
 }
+
+// Example usage
+const exampleDisassembly = [
+  0xc7, 0x06, 0xe8, 0x03, 0x01, 0x00, 0xc7, 0x06, 0xea, 0x03, 0x02, 0x00, 0xc7,
+  0x06, 0xec, 0x03, 0x03, 0x00, 0xc7, 0x06, 0xee, 0x03, 0x04, 0x00, 0xbb, 0xe8,
+  0x03, 0xc7, 0x47, 0x04, 0x0a, 0x00, 0x8b, 0x1e, 0xe8, 0x03, 0x8b, 0x0e, 0xea,
+  0x03, 0x8b, 0x16, 0xec, 0x03, 0x8b, 0x2e, 0xee, 0x03,
+]
+
+const cpu = createCPU()
+decodeAndExecute(cpu, exampleDisassembly)
