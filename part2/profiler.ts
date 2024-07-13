91fea487a0b0b17a5358e0b9faa5840571b9ac4d
ahmad anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2024-07-13 15:05:17 +1200

    improve profiler and add calculations

--- part2/profiler.ts
+++ part2/profiler.ts
@@ -1,47 +1,73 @@
 import { readCPUTimer } from './perf/os-metrics'
 
-interface PerformanceMetrics {
-  timeElapsed: bigint
-  throughput: number
-  operationsPerSecond: number
-}
-
-export function makeProfiler() {
+export function makeProfiler(freq: bigint) {
   let start: bigint
-  let _size: number
-  let timeElapsed: bigint
+
+  const anchors = new Map<
+    string,
+    { size: number; start: bigint; elapsed?: bigint }
+  >()
 
   return Object.freeze({
     beginTime,
     endTime,
-    getMetrics,
     printMetrics,
   })
 
-  function beginTime(size: number = 0): void {
-    start = readCPUTimer()
-    _size = size
-  }
-
-  function endTime(): void {
-    timeElapsed = readCPUTimer() - start
-  }
-
-  function getMetrics(): PerformanceMetrics {
-    const elapsedSeconds = Number(timeElapsed) / (2.3 * 1e9) // Assuming 2.3 GHz CPU, adjust as needed
-    return {
-      timeElapsed,
-      throughput: _size / elapsedSeconds,
-      operationsPerSecond: _size / elapsedSeconds,
+  function beginTime(label: string, sizeInBytes: number = 0): void {
+    if (anchors.has(label)) {
+      throw new Error(`Label ${label} already exists`)
     }
+    start = readCPUTimer()
+    anchors.set(label, { start, size: sizeInBytes })
   }
 
-  function printMetrics(): void {
-    const metrics = getMetrics()
-    console.table({
-      timeElapsed: metrics.timeElapsed.toString(),
-      throughput: metrics.throughput.toFixed(2),
-      operationsPerSecond: metrics.operationsPerSecond.toFixed(2),
-    })
+  function endTime(label: string): void {
+    const anchor = anchors.get(label)
+    if (!anchor) {
+      throw new Error(`Label ${label} does not exist`)
+    }
+    anchor.elapsed = readCPUTimer() - BigInt(anchor.start)
   }
+
+  function printMetrics() {
+    let totalTimeElapsed = BigInt(0)
+    let totalSize = 0
+
+    const table = Array.from(anchors.entries()).map(
+      ([label, { elapsed, size }]) => {
+        if (elapsed) {
+          totalTimeElapsed += elapsed
+          totalSize += size
+          const timeInSeconds = Number(elapsed) / Number(freq)
+          const throughput =
+            timeInSeconds > 0 ? toGigabytes(size) / timeInSeconds : 0
+          const percent = (Number(elapsed) / Number(totalTimeElapsed)) * 100
+
+          return {
+            label,
+            elapsed,
+            percent: Number(percent.toFixed(2)),
+            throughput: Number(throughput.toFixed(2)),
+          }
+        }
+      },
+    )
+
+    const totalTimeInSeconds = Number(totalTimeElapsed) / Number(freq)
+    const overallThroughput =
+      totalTimeInSeconds > 0 ? toGigabytes(totalSize) / totalTimeInSeconds : 0
+
+    console.log(
+      'Total time elapsed:',
+      (totalTimeInSeconds * 1000).toFixed(2),
+      'ms',
+    )
+    console.log('Overall Throughput:', overallThroughput.toFixed(2), 'gb/s')
+    console.table(table)
+  }
+}
+
+function toGigabytes(bytes: number): number {
+  return bytes / 1024 / 1024 / 1024
 }
