1f5b92ccd6e535a3ecff61e5fed91835d033ffbb
ahmad anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2024-07-13 16:45:44 +1200

    profile sum of haversine distances

--- part2/75-timed-haversine.ts
+++ part2/75-timed-haversine.ts
@@ -8,27 +8,45 @@ import { makeProfiler } from './profiler'
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname)
 
+const freq = estimateCPUTimerFreq()
+const profiler = makeProfiler(freq)
+
 function main() {
-  const freq = estimateCPUTimerFreq()
   const filePath = path.join(__dirname, 'data/pairs.json')
   const stat = fs.statSync(filePath)
   console.log(`File size: ${stat.size} bytes`)
-  const profiler = makeProfiler(freq)
 
-  profiler.beginTime('Read', stat.size)
-  const jsonString = fs.readFileSync(filePath, 'utf-8')
+  profiler.beginTime('Read')
+  const inputString = fs.readFileSync(filePath, 'utf-8')
   profiler.endTime('Read')
 
-  profiler.beginTime('Parse')
-  const data = parse(jsonString)
+  profiler.beginTime('Parse', stat.size)
+  const jsonData = parse(inputString)
   profiler.endTime('Parse')
 
-  const pair = data.pairs[0]
-
-  profiler.beginTime('Haversine')
-  const haversine = ReferenceHaversine(pair.x0, pair.y0, pair.x1, pair.y1)
-  profiler.endTime('Haversine')
+  const sum = sumHaversineDistances(jsonData.pairs)
+  console.log(`Actual sum: ${sum}`)
 
   profiler.printMetrics()
 }
+
+function sumHaversineDistances(
+  pairs: Array<{ x0: number; y0: number; x1: number; y1: number }>
+) {
+  // 8 because each number is 8 bytes
+  const sizeOfNumberType = 8
+  // 4 because each pair has 4 numbers (x0, y0, x1, y1)
+  const sizeOfPair = sizeOfNumberType * 4
+  // bytes is the total size of the data (you can also use the size of the file)
+  const bytes = pairs.length * sizeOfPair
+  profiler.beginTime('Haversine', bytes)
+  let sum = 0
+  let sumCoef = 1 / pairs.length
+  for (const pair of pairs) {
+    sum += ReferenceHaversine(pair.x0, pair.y0, pair.x1, pair.y1) * sumCoef
+  }
+  profiler.endTime('Haversine')
+  return sum
+}
+
 main()
