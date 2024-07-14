53170ee07f7b0772066fb32ccb5644fa03af7d7b
ahmad anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2024-07-15 05:14:12 +1200

    implement reptition testing

---
+++ part2/103-reptition-testing.ts
@@ -0,0 +1,52 @@
+import fs from 'node:fs'
+import path from 'node:path'
+
+import { parse } from './parser/json-parser'
+import { estimateCPUTimerFreq } from './perf/os-metrics'
+import { makeProfiler } from './profiler'
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
+const freq = estimateCPUTimerFreq()
+const profiler = makeProfiler(freq)
+
+function main() {
+  const filePath = path.join(__dirname, 'data/pairs.json')
+  const stat = fs.statSync(filePath)
+  const inputString = fs.readFileSync(filePath, 'utf-8')
+
+  // time to wait
+  const secondsToWait = 10
+  const startTimer = Date.now()
+  let minElapsed = Number.MAX_VALUE
+  let maxElapsed = 0
+  let sumElapsed = 0
+  let testsCounter = 0
+  for (;;) {
+    testsCounter++
+
+    profiler.beginTime('Parse', stat.size)
+    const jsonData = parse(inputString)
+    const elapsed = profiler.endTime('Parse')
+
+    minElapsed = Math.min(minElapsed, Number(elapsed))
+    maxElapsed = Math.max(maxElapsed, Number(elapsed))
+    sumElapsed += Number(elapsed)
+
+    const endTimer = Date.now()
+    const testElapsed = endTimer - startTimer
+
+    if (passedWaitTime(secondsToWait, testElapsed)) break
+  }
+
+  console.table({
+    min: minElapsed,
+    avg: sumElapsed / testsCounter,
+    max: maxElapsed,
+  })
+}
+
+function passedWaitTime(secondsToWait: number, timeElapsed: number) {
+  return timeElapsed / 1000 > secondsToWait
+}
+
+main()
