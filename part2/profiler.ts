b38ea9a1f0224be1d14f33f3b036da2f9d1d1a6e
ahmad anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2024-07-15 05:26:12 +1200

    return metrics

--- part2/profiler.ts
+++ part2/profiler.ts
@@ -9,6 +9,7 @@ export function makeProfiler(freq: bigint) {
   return Object.freeze({
     beginTime,
     endTime,
+    getMetrics,
     printMetrics,
   })
 
@@ -25,6 +26,44 @@ export function makeProfiler(freq: bigint) {
     anchor.elapsed = readCPUTimer() - anchor.start
 
     return anchor.elapsed || 0
+  }
+
+  function getMetrics() {
+    let totalTimeElapsed = BigInt(0)
+    const megabyte = 1024 * 1024
+
+    const table = Array.from(anchors.entries()).map(
+      ([label, { elapsed, processedByteCount }]) => {
+        totalTimeElapsed += elapsed || BigInt(0)
+        processedByteCount += processedByteCount
+        const timeInSeconds = Number(elapsed) / Number(freq)
+        const throughput =
+          timeInSeconds > 0
+            ? toGigabytes(processedByteCount) / timeInSeconds
+            : 0
+        const percent = (Number(elapsed) / Number(totalTimeElapsed)) * 100
+
+        return {
+          label,
+          elapsed,
+          percent: Number(percent.toFixed(2)),
+          megabytes: processedByteCount / megabyte,
+          throughput: throughput,
+        }
+      },
+    )
+
+    const totalTimeInSeconds = Number(totalTimeElapsed) / Number(freq)
+    const overallThroughput = table.reduce(
+      (acc, r) => acc + (r && r.throughput) || 0,
+      0,
+    )
+
+    return {
+      totalTimeInSeconds,
+      overallThroughput,
+      table,
+    }
   }
 
   function printMetrics() {
@@ -49,19 +88,19 @@ export function makeProfiler(freq: bigint) {
           megabytes: processedByteCount / megabyte,
           throughput: throughput,
         }
-      }
+      },
     )
 
     const totalTimeInSeconds = Number(totalTimeElapsed) / Number(freq)
     const overallThroughput = table.reduce(
       (acc, r) => acc + (r && r.throughput) || 0,
-      0
+      0,
     )
 
     console.log(
       'Total time elapsed:',
       (totalTimeInSeconds * 1000).toFixed(2),
-      'ms'
+      'ms',
     )
     console.log('Overall Throughput:', overallThroughput.toFixed(2), 'gb/s')
     console.table(table)
